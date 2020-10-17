f999c9ac63fadc2c20566cd1ce6c3054f56cb198
Fri2ends <63165093+Fri2ends@users.noreply.github.com>
2020-10-17 23:01:08 +0330

    Start Project

---
+++ Fri2Ends.ORM/Fri2Ends.ORM/DBConnection/FRMconnection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Text;
+using Dapper;
+
+namespace Fri2Ends.ORM.DBConnection
+{
+    public class FrMconnection 
+    {
+        SqlConnection sqlConnection = new SqlConnection("data source=.;initial catalog = fri2ends;integrated security = true");
+
+        private IDbCommand cmd;
+        public void te()
+        {
+            
+        }
+    }
+
+    public static class t
+    {
+        public static int Query(this IDbConnection connection)
+        {
+            connection.Query<>()
+        }
+    }
+}
